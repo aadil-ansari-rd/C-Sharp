f1d50728877c26faac65af7dd2bb3e6fea46e921
aadil-ansari-rd <aadilansari.rd@gmail.com>
2025-06-23 11:27:34 +0530

    update

---
+++ 12 Explicit Implementation in Interfaces/12 Explicit Implementation in Interfaces/Program.cs
@@ -0,0 +1,74 @@
+﻿interface IC1
+{
+    void Print();
+}
+interface IC2
+{
+    void Print();
+}
+class Program : IC1, IC2
+{
+    public void Print() //Default Implementation : Only access by class type reference, not by interface type reference
+    {
+        Console.WriteLine("Default Print");
+    }
+    void IC1.Print() //Particular Implementation : Only access by interface type reference, not by class type reference
+    {
+        Console.WriteLine("Secific IC1");
+    }
+    void IC2.Print()
+    {
+        Console.WriteLine("Secific IC2");
+    }
+    public static void Main()
+    {
+        Program program = new Program();
+        program.Print();
+        ((IC1)program).Print();
+        ((IC2)program).Print();
+
+
+        IC1 c1 = new Program();
+        c1.Print();
+
+        IC2 c2 = new Program();
+        c2.Print();
+
+    }
+}
+
+
+//----------------------------------------------------------------------------------
+
+
+
+//interface IC1
+//{
+//    void Print();
+//}
+//interface IC2
+//{
+//    void Print();
+//}
+//class Program : IC1, IC2
+//{
+//    public void Print()
+//    {
+//        Console.WriteLine("Default Print");
+//    }
+//    public static void Main()
+//    {
+//        Program program = new Program();
+//        program.Print();
+//        ((IC1)program).Print();
+//        ((IC2)program).Print();
+
+
+//        IC1 c1 = new Program(); 
+//        c1.Print();
+
+//        IC2 c2 = new Program();
+//        c2.Print();
+
+//    }
+//}
