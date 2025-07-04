0eaa0522e3b05ca5818f59336828b7750fe8ff1b
aadil-ansari-rd <aadilansari.rd@gmail.com>
2025-07-04 10:51:25 +0530

    update

---
+++ 21 Custom Exception Class/21 Custom Exception Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            throw new UserAlreadyLoggedInException("User already logged in. Duplicate session is not alowed");
+        }catch(UserAlreadyLoggedInException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+[Serializable]
+class UserAlreadyLoggedInException : Exception
+{
+    public UserAlreadyLoggedInException() : base() { }
+    public UserAlreadyLoggedInException(string message) : base(message) { }
+
+    public UserAlreadyLoggedInException(string message, Exception innerException) : base(message, innerException) { }
+
+    public UserAlreadyLoggedInException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
