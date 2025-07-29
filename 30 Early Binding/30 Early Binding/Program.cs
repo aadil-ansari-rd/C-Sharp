d8a59a481d486b177d9874247acbab3d98d0feff
aadil-ansari-rd <aadilansari.rd@gmail.com>
2025-07-29 12:41:33 +0530

    update

---
+++ 30 Early Binding/30 Early Binding/Program.cs
@@ -0,0 +1,17 @@
+﻿class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator(); // Known at compile time
+        int result = calc.Add(10, 20);      // Early binding
+        Console.WriteLine("Sum: " + result);
+    }
+}
---
+++ 31 Late Binding/31 Late Binding/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace LateBindingExample
+{
+    public class Calculator
+    {
+        public void Greet(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Load the current assembly (you can also load from DLL)
+            Assembly asm = Assembly.GetExecutingAssembly();
+
+            // Get type information
+            Type calcType = asm.GetType("LateBindingExample.Calculator");
+
+            // Create an instance dynamically
+            object calcInstance = Activator.CreateInstance(calcType);
+
+            // Invoke Greet method (void method)
+            MethodInfo greetMethod = calcType.GetMethod("Greet");
+            greetMethod.Invoke(calcInstance, new object[] { "Aadil" });
+
+            // Invoke Add method (returns int)
+            MethodInfo addMethod = calcType.GetMethod("Add");
+            object result = addMethod.Invoke(calcInstance, new object[] { 5, 7 });
+
+            Console.WriteLine("Addition Result: " + result);
+        }
+    }
+}
